f78666e2389752e2a00258ad7a3ef3b168df1ea3
Przemysław Przymusiński <p.przymusinski@gmail.com>
2026-05-31 19:54:40 +0200

    Normalizacja predkosci gracza

--- player.cpp
+++ player.cpp
@@ -1,4 +1,5 @@
 #include "player.h"
+#include <cmath>
 
 Player::Player() {
     speed = 300.f;
@@ -9,29 +10,36 @@ Player::Player() {
     sprite.setTexture(texture);
 
     // temp
-    sprite.setScale(0.6f, 0.6f);
+    sprite.setScale(0.6, 0.6);
     sprite.rotate(180);
 
-    sprite.setPosition(300.f, 700.f);
+    sprite.setPosition(300.0, 700.0);
 }
 
 void Player::handleInput(float deltaTime) {
-        sf::Vector2f movement(0.f, 0.f);
+    sf::Vector2f movement(0.0, 0.0);
 
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            movement.x -= speed * deltaTime;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            movement.x += speed * deltaTime;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            movement.y -= speed * deltaTime;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            movement.y += speed * deltaTime;
-        }
-        sprite.move(movement);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+        movement.x -= speed * deltaTime;
     }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+        movement.x += speed * deltaTime;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+        movement.y -= speed * deltaTime;
+    }
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+        movement.y += speed * deltaTime;
+    }
+
+    // normalizowie wektora zeby ukosny ruch byl odpowiedniej predkosci
+    float length = std::sqrt(movement.x * movement.x + movement.y * movement.y);
+    if (length != 0.f) {
+        movement /= length;
+    }
+
+    sprite.move(movement * speed * deltaTime);
+}
 
 void Player::draw(sf::RenderWindow& window) {
     window.draw(sprite);
