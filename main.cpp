7c31a55ab5852dd314d4263d335135096c361905
Ann-018 <antonina.pietrucik@student.put.poznan.pl>
2026-06-07 11:52:50 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,47 @@
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <game.h>
+#include "przyciski.h"
 
 int main() {
-    Game game;
-    game.run();
+    przyciski* Przyciski[4];
+    sf::Texture tekstury[4];
+    tekstury[0].loadFromFile("przycisk1.png");
+    tekstury[1].loadFromFile("przycisk2.png");
+    tekstury[2].loadFromFile("przycisk3.png");
+    tekstury[3].loadFromFile("przycisk4.png");
+    create_bottoms(Przyciski, tekstury, 1);
+
+    sf::Texture backgroundTexture;
+    backgroundTexture.loadFromFile("start2.png");
+    sf::Sprite backgroundSprite(backgroundTexture);
+    backgroundSprite.setScale(0.5,0.5);
+
+    sf::RenderWindow window(sf::VideoMode(660, 1056), "Void defender");
+    window.setFramerateLimit(60);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            if(event.type == sf::Event::MouseButtonPressed){
+                for(auto &p: Przyciski){
+                    p->handleEvent(event, window);
+                }
+            }
+        }
+        window.clear();
+        window.draw(backgroundSprite);
+        for(const auto &p: Przyciski){
+            window.draw(*p);
+        }
+        window.display();
+    }
+
+    for(int i = 0; i < 4; ++i) {
+        delete Przyciski[i];
+    }
 
     return 0;
 }
