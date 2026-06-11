c2d9d9b41d5ae5973e292f9602c723c64be0790f
Ann-018 <antonina.pietrucik@student.put.poznan.pl>
2026-06-12 01:35:24 +0200

    Update opponent.cpp
    dodanie animacji

--- opponent.cpp
+++ opponent.cpp
@@ -18,10 +18,6 @@ Opponent::Opponent(const sf::Texture& texture, float _x) {
     sprite.setPosition(_x, -50.0);
 }
 
-void Opponent::draw(sf::RenderWindow& window) const {
-    window.draw(sprite);
-}
-
 sf::FloatRect Opponent::getBounds() const {
     return sprite.getGlobalBounds();
 }
@@ -39,4 +35,13 @@ void Opponent::destroy() {
 
 bool Opponent::isDestroyed() const {
     return destroyed;
+}
+
+void Opponent:: setAnimation(){
+    add_animation_frame(sf::IntRect(0,0,330, 414), rectangles);
+    add_animation_frame(sf::IntRect(408,0,330,414), rectangles);
+}
+
+std::vector<sf::IntRect>& Opponent:: getRecte(){
+    return rectangles;
 }
