884fdce3fdda3aea57e84ebabd3e557316b5b700
Ann-018 <antonina.pietrucik@student.put.poznan.pl>
2026-06-12 01:46:26 +0200

    Update player.h
    dodanie metod (setAnimation, updateAnimation), vectorów (idleFrame, leftFrame, rightFrame)

--- player.h
+++ player.h
@@ -1,11 +1,17 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <SFML/Graphics.hpp>
+#include "animation.h"
+
+enum class PlayerState {
+    IDLE,       // Lot prosto
+    LEFT,       // Skręt w lewo
+    RIGHT       // Skręt w prawo
+};
 
 class Player
 {
 private:
-    sf::Sprite sprite;
     sf::Texture texture;
     float speed;
     int lifes;
@@ -18,11 +24,19 @@ private:
     sf::Clock damageTimer;
     float invincibilityDuration = 1.0f;
     bool isInvincible = false;
+
+    PlayerState currentState = PlayerState::IDLE;
+    std::vector<sf::IntRect> idleFrames;
+    std::vector<sf::IntRect> leftFrames;
+    std::vector<sf::IntRect> rightFrames;
+
+    void setAnimation();
 public:
     Player();
 
     void handleInput(float deltaTime, sf::Vector2f windowSize);
     void draw(sf::RenderWindow& window);
+    void updateAnimation(const sf::Time& elapsed);
 
     // upgrade'y
     void increaseSpeed();
