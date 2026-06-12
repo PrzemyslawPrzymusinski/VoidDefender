fbda111477e65b2ec6431b48923c3ce79c3584c1
Ann-018 <antonina.pietrucik@student.put.poznan.pl>
2026-06-12 23:37:03 +0200

    Update basicopp.cpp

--- basicopp.cpp
+++ basicopp.cpp
@@ -4,18 +4,12 @@ BasicOpp::BasicOpp(const sf::Texture& texture) : Opponent(texture) {
     speed = rand() % 200;
     lifes = 1;
     points = 1;
-
-    setAnimation();
-
-    setScale(0.3, 0.3);
 }
 
 BasicOpp::BasicOpp(const sf::Texture& texture, float _x) : Opponent(texture, _x) {
     speed = rand() % 150 + 100;
     points = 1;
     lifes = 1;
-
-    setScale(0.3, 0.3);
 }
 
 void BasicOpp::movement(float deltaTime) {
